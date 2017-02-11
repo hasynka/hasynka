1312c325de1a003839ce9101579c865e2dcb72b3
student4 <student4@pcoip.ki.agh.edu.pl>
2017-02-11 15:03:29 +0100

    selekcja

--- SomeProgram.java
+++ SomeProgram.java
@@ -3,7 +3,10 @@ public class SomeProgram {
     public static void main(String[] args) {
         System.out.println("cokolwiek");
     }
-}
+	public void startRocket( {
+	// TODO implement
+} 
+
 public void manageTheSatellite() {
     // TODO implement
 }
